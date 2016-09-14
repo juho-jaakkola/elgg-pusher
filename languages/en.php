ea8a18d4b833c6cac1bbf07f717998dbecf46216
Steve Clay <steve@mrclay.org>
2016-09-14 12:17:14 -0400

    chore(general): clarifies installation and settings
    Swaps settings order and gives them clearer names and descriptions.
    Makes public port optional.
    Removes mention of `nohup` since this doesn't seem like a realistic
    production solution.

--- languages/en.php
+++ languages/en.php
@@ -1,10 +1,12 @@
 <?php
 
 return array(
+	'pusher:settings:private_port:label' => 'Internal Web socket port',
+	'pusher:settings:private_port:desc' => 'The port to which the push server will listen for web socket connections.',
+
 	'pusher:settings:public_port:label' => 'Public web socket port',
-	'pusher:settings:public_port:desc' => 'The server port that clients will use to establish a websocket connection to the push server.',
-	'pusher:settings:private_port:label' => 'Private web socket port',
-	'pusher:settings:private_port:desc' => 'This allows you to define a separate port that the push server will listen to, if you are using a proxy to tunnel traffic from the public port. Enter the public web socket port, if proxy is not being used.',
+	'pusher:settings:public_port:desc' => 'Leave empty if your web socket port is public. If you are using a proxy to tunnel traffic, enter the external port clients must use.',
+
 	'pusher:settings:scheme:label' => 'URI scheme',
 	'pusher:settings:scheme:insecure' => 'ws:// (insecure)',
 	'pusher:settings:scheme:secure' => 'wss:// (secure)',
--- views/default/plugins/pusher/settings.php
+++ views/default/plugins/pusher/settings.php
@@ -4,25 +4,27 @@
  */
 
 $plugin = elgg_extract('entity', $vars);
-
-$public_port_label = elgg_echo('pusher:settings:public_port:label');
-$public_port_input = elgg_view('input/text', array(
-	'name' => 'params[public_port]',
-	'value' => $plugin->public_port,
-));
-$public_port_desc = elgg_echo('pusher:settings:public_port:desc');
+/* @var ElggPlugin $plugin */
 
 $private_port_label = elgg_echo('pusher:settings:private_port:label');
 $private_port_input = elgg_view('input/text', array(
 	'name' => 'params[private_port]',
-	'value' => $plugin->private_port,
+	'value' => $plugin->getSetting('private_port', '8099'),
+	'required' => true,
 ));
 $private_port_desc = elgg_echo('pusher:settings:private_port:desc');
+
+$public_port_label = elgg_echo('pusher:settings:public_port:label');
+$public_port_input = elgg_view('input/text', array(
+	'name' => 'params[public_port]',
+	'value' => $plugin->getSetting('public_port'),
+));
+$public_port_desc = elgg_echo('pusher:settings:public_port:desc');
 
 $scheme_label = elgg_echo('pusher:settings:scheme:label');
 $scheme_input = elgg_view('input/radio', array(
 	'name' => 'params[scheme]',
-	'value' => $plugin->scheme,
+	'value' => $plugin->getSetting('scheme'),
 	'options' => array(
 		elgg_echo('pusher:settings:scheme:secure') => 'wss://',
 		elgg_echo('pusher:settings:scheme:insecure') => 'ws://',
@@ -32,14 +34,14 @@ $scheme_desc = elgg_echo('pusher:settings:scheme:desc');
 
 echo <<<FORM
 	<div>
-		<label>$public_port_label</label>
-		$public_port_input
-		<p class="elgg-text-help">$public_port_desc</p>
-	</div>
-	<div>
 		<label>$private_port_label</label>
 		$private_port_input
 		<p class="elgg-text-help">$private_port_desc</p>
+	</div>
+	<div>
+		<label>$public_port_label</label>
+		$public_port_input
+		<p class="elgg-text-help">$public_port_desc</p>
 	</div>
 	<div>
 		<label>$scheme_label</label>
